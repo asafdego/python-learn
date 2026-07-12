d0ebbe8e85eaf95ce6408c26f8406e34ab60b2f0
Asaf-Dego-Aorus-WSL <asafdego@gmail.com>
2026-07-12 19:49:25 +0300

    fix code for 'else:' to use only 3 char

--- task1.py
+++ task1.py
@@ -1,9 +1,9 @@
 
-majin_list = ["kid_buu", "majin_buu", "super_buu", "evil_buu", "majin_vegeta"]
+majin_list = ["kid_buu", "majin_buu", "super_buu", "evil_buu", "majin_vegeta", "asaf dego"]
 
 for majin in majin_list:
-    if majin.find("_"):
+    if majin.find("_") != -1:
         print (majin + " " + majin[:majin.find("_")])
     
     else:
-        print (majin[:3])
+        print (majin + " " + majin[0:3])
