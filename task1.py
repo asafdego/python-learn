8c12eda94c5ebfb7de85b8d610a3754f51403f86
Asaf-Dego-Aorus-WSL <asafdego@gmail.com>
2026-07-12 18:49:47 +0300

    Added to code full name + name until '_'

--- task1.py
+++ task1.py
@@ -2,4 +2,8 @@
 majin_list = ["kid_buu", "majin_buu", "super_buu", "evil_buu", "majin_vegeta"]
 
 for majin in majin_list:
-    print (majin + " " +majin[0:3])
+    if majin.find("_"):
+        print (majin + " " + majin[:majin.find("_")])
+    
+    else:
+        print (majin[:3])
