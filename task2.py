7175ab6c5a3f8e5d6816b81e2ed907b37b9a38fb
Asaf-Dego-Aorus-WSL <asafdego@gmail.com>
2026-07-14 19:07:18 +0300

    Variable Initialization Best Practice update

--- task2.py
+++ task2.py
@@ -1,14 +1,14 @@
 
 number = [10, 12, 3, 45, 74, 67, 34]
 
-bigest = number[0]
-sec_bigest = number[0]
+bigest = float('-inf')
+sec_bigest = float('-inf')
 
 for num in number:
     if num > bigest:
         sec_bigest = bigest
         bigest = num
-        
+
     elif bigest > num > sec_bigest :
         sec_bigest = num
 
